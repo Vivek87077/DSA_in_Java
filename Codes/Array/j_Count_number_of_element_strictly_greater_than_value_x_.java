2b5eed68f8075e80dfb6f14e532ce245cc0016f2
Vivek Shrama <vsharma87077@gmail.com>
2024-02-26 15:52:02 +0530

    done

--- Codes/Array/j_Count_number_of_element_strictly_greater_than_value_x_.java
+++ Codes/Array/j_Count_number_of_element_strictly_greater_than_value_x_.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 
 public class j_Count_number_of_element_strictly_greater_than_value_x_ {
+    static int greaterElement(int[] arr, int x){
+        int count = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > x){
+                count++ ;
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
         System.out.println("Enter the size of the array: ");
         try (Scanner sc = new Scanner(System.in)) {
@@ -14,14 +23,8 @@ public class j_Count_number_of_element_strictly_greater_than_value_x_ {
 
             System.out.println("Enter the value of X: ");
             int x = sc.nextInt();
-
-            int count = 0;
-            for(int i = 0; i < array.length; i++){
-                if(array[i] > x){
-                    count++ ;
-                }
-            }
-            System.out.println("Number of elements strictly greater than value x: "+count);
+            
+            System.out.println("Number of elements strictly greater than value x: "+ greaterElement(array, x));
 
         }
     }
